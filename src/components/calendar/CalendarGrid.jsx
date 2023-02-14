25ce08c4ed45322268079ca96a5834f79c4b84a8
SlothChuck <markbelyy@yandex.ru>
2023-02-14 19:32:19 +0300

    rewind and fullclick

--- src/components/calendar/CalendarGrid.jsx
+++ src/components/calendar/CalendarGrid.jsx
@@ -39,14 +39,25 @@ const DayWrapper = styled.div`
     justify-content: center
 `
 const CurrentDay = styled.div`
-color: ${props => !props.isThisMonth ? 'gray' : ''}
+    color: ${props => !props.isThisMonth ? 'gray' : ''}
+`
+const DateDN = styled.p`
+    display: none;
 `
 
 export default function CalendarGrid(props) {
 
-    // const [ignoreon, setIgnoreOn] = useState(0)
-    // const [ignoreoff, setIgnoreOff] = useState(0)
+    const [startDateOn, setStartDateOn] = useState(null);
+    const [endDateOn, setEndDateOn] = useState(null);
+    const [startDateOff, setStartDateOff] = useState(null);
+    const [endDateOff, setEndDateOff] = useState(null);
+
+    // const IgnoreOnDays = []
+    // const IgnoreOffDays = []
+    // const PatternDays = []
+
     const [isRed, setIsRed] = useState([])
+
     const day = props.startDay.clone()
     const daysArray = [...Array(42)].map(() => day.add(1, 'd').clone())
 
@@ -55,34 +66,40 @@ export default function CalendarGrid(props) {
         let arr = daysArray.filter(item => item.day() === dayweek)
         arr = arr.filter(item => item.format('M') === daysArray[20].format('M'))
         console.log(arr)
-        return arr.map(x => +x)
+        return arr.map(x => x.format('DDMMYYYY'))
     }
     const getYearPatternDays = (dayweek, daysArray) => {
-        const yearday = props.startDay.clone()
-        console.log('первый день в году', yearday.endOf('year').diff(yearday.startOf('year'), 'days'))
-
         let arr = daysArray.filter(item => item.day() === dayweek)
         console.log(arr)
-        return arr.map(x => +x)
+        return arr.map(x => x.format('DDMMYYYY'))
     }
-    // const getIgonoreDays = (firstday, lastday) => {
-    //     let n = lastday.diff(firstday, 'days')
-    //     firstday.subtract(1, 'd')
-    //     let arr = [...Array(n + 1)].map(() => firstday.add(1, 'd').clone().format('D'))
-    //     console.log(arr)
-    //     return arr
-    // }
-    useEffect(() =>{
-        const selecteddays = document.querySelectorAll('.cellwrapper');
-        console.log(selecteddays);
-        selecteddays.forEach(item => {
-            item.addEventListener('click', function handleClick(event) {
-                console.log('box clicked', event);
-                if (document.querySelector('.selectignore:checked').value == 3) item.setAttribute('style', 'background-color: yellow;');
-                else if (document.querySelector('.selectignore:checked').value == 4) item.setAttribute('style', 'background-color: blue;');
-            });
-        });
-    })
+    const getIgonoreDays = (firstday, lastday) => {
+        let n = lastday.diff(firstday, 'days')
+        firstday.subtract(1, 'd')
+        let arr = [...Array(n + 1)].map(() => firstday.add(1, 'd').clone().format('DDMMYYYY'))
+        console.log(arr)
+        return arr
+    }
+
+    useEffect(() => {
+        if (startDateOn && endDateOn) {
+            let arron = getIgonoreDays(startDateOn, endDateOn)
+            console.log('arron:', arron)
+            let arr = isRed.filter(item => !arron.includes(item))
+            setIsRed(arr)
+            console.log('isRed:', isRed)
+        }
+    }, [startDateOn, endDateOn])
+
+    useEffect(() => {
+        if (startDateOff && endDateOff) {
+            let arroff = getIgonoreDays(startDateOff, endDateOff)
+            console.log('arroff:', arroff)
+            let arr = arroff.filter(item => !isRed.includes(item))
+            setIsRed(isRed.concat(arr))
+            console.log('isRed:', isRed)
+        }
+    }, [startDateOff, endDateOff])
 
 
     const OnClickCell2 = (weekday, day) => {
@@ -92,23 +109,46 @@ export default function CalendarGrid(props) {
             let selectmode = +elems.value
             if (selectmode === 1) {
                 console.log("Задаем паттерн")
-                let a = getPatternDays(weekday, daysArray)
-                setIsRed(prev => prev.concat(a).filter((x, i) => isRed.concat(a).indexOf(x) === i));
+                let arrp = getPatternDays(weekday, daysArray)
+                setIsRed(prev => prev.concat(arrp).filter((x, i) => isRed.concat(arrp).indexOf(x) === i));
                 console.log(`isRed:${isRed}`)
 
 
             } else if (selectmode === 2) {
                 console.log("Задаем паттерн")
-                let a = getYearPatternDays(weekday, daysArray)
-                setIsRed(prev => prev.concat(a).filter((x, i) => isRed.concat(a).indexOf(x) === i));
+                let arryp = getYearPatternDays(weekday, daysArray)
+                setIsRed(prev => prev.concat(arryp).filter((x, i) => isRed.concat(arryp).indexOf(x) === i));
                 console.log(`isRed:${isRed}`)
 
 
             } else if (selectmode === 3) {
                 console.log("Задаем рабочие")
+                if (!startDateOn) {
+                    console.log("Первый день1:", day)
+                    setStartDateOn(day);
+                } else if (!endDateOn) {
+                    console.log("Второй день:", day)
+                    setEndDateOn(day);
+                } else {
+                    console.log("Первый день2:", day)
+                    setStartDateOn(day);
+                    setEndDateOn(null);
+                }
+                
             } else if (selectmode === 4) {
                 console.log("Задаем выходные")
 
+                if (!startDateOff) {
+                    console.log("Первый день1:", day)
+                    setStartDateOff(day);
+                } else if (startDateOff && !endDateOff) {
+                    console.log("Второй день:", day)
+                    setEndDateOff(day);
+                } else {
+                    console.log("Первый день2:", day)
+                    setStartDateOff(day);
+                    setEndDateOff(null);
+                }
             }
         }
     }
@@ -125,19 +165,22 @@ export default function CalendarGrid(props) {
                     daysArray.map((dayItem) => (
                         <CellWrapper onClick={() => {
                             // OnClickCell(dayItem.day(), +dayItem.format('D'));
-                            OnClickCell2(dayItem.day(), dayItem.format('D'));
+                            OnClickCell2(dayItem.day(), dayItem);
                         }}
 
                             key={dayItem.format('DDMMYYYY')}
                             isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
-                            isDayRed={isRed.includes(+dayItem)}
+                            isDayRed={isRed.includes(dayItem.format('DDMMYYYY'))}
+                            id={dayItem.format('DDMMYYYY')}
                         >
-                            <RowCell flexend>
+                            <RowCell id={dayItem.format('DDMMYYYY')} flexend>
                                 <DayWrapper
                                     isCurentDay={dayItem.format('DDMMYYYY') === moment().format('DDMMYYYY')}
+                                    id={dayItem.format('DDMMYYYY')}
                                 >
                                     <CurrentDay
                                         isThisMonth={dayItem.format('M') === daysArray[20].format('M')}
+                                        id={dayItem.format('DDMMYYYY')}
                                     >
                                         {dayItem.format('D')}
                                     </CurrentDay>
