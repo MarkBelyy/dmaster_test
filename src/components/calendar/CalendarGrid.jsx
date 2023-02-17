d978bd4d6af3db398fd0e9e2b75c26f43dab3874
SlothChuck <markbelyy@yandex.ru>
2023-02-17 14:14:02 +0300

    all start arr

--- src/components/calendar/CalendarGrid.jsx
+++ src/components/calendar/CalendarGrid.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 import _ from 'lodash';
 import styled from 'styled-components'
-
+import api from '../../services/api'
 
 const GridWrapper = styled.div`
     display: grid;
@@ -66,7 +66,7 @@ export default function CalendarGrid(
     const getPatternDays = (dayweek, daysArray) => {
         let arr = daysArray.filter(item => item.day() === dayweek)
         arr = arr.filter(item => item.format('M') === daysArray[20].format('M'))
-       
+
         return arr.map(x => x.format('DDMMYYYY'))
     }
     const getYearPatternDays = (dayweek, daysArray) => {
@@ -91,6 +91,14 @@ export default function CalendarGrid(
         }
         return uobj
     }
+    const parseDate = (dateObj) => {
+
+        const startDate = moment(`${dateObj.startyear}-${dateObj.startmonth}-${dateObj.startday}`, "YYYY-MM-DD");
+        const endDate = moment(`${dateObj.endyear}-${dateObj.endmonth}-${dateObj.endday}`, "YYYY-MM-DD");
+        console.log('startdata', startDate); // Выводит "05.02.2023"
+        console.log('enddata', endDate); // Выводит "19.02.2023"
+        return [startDate, endDate];
+    }
 
     useEffect(() => {
         setIsRed([])
@@ -106,20 +114,37 @@ export default function CalendarGrid(
             year: 2023,
             month: 13,
             day: 4
-            }, {
-                year: 2023,
-                month: 2,
-                day: 5
-            }]
+        }, {
+            year: 2023,
+            month: 2,
+            day: 5
+        }]
+        let ionget = [{
+            startday: 5,
+            startmonth: 2,
+            startyear: 2023,
+            endday: 19,
+            endmonth: 2,
+            endyear: 2023
+        }]
+        let ioffget = [{
+            startday: 3,
+            startmonth: 2,
+            startyear: 2023,
+            endday: 25,
+            endmonth: 2,
+            endyear: 2023
+        }]
         console.log(checkObjSinArray(patget, patternDays))
         setPatternDays(patternDays.concat(checkObjSinArray(patget, patternDays)))
-
+        setIgnoreOnDays(ignoreOnDays.concat(checkObjSinArray(ionget, ignoreOnDays)))
+        setIgnoreOffDays(ignoreOffDays.concat(checkObjSinArray(ioffget, ignoreOffDays)))
         // setSuccesful(true)
     }, [tempday])
 
 
     useEffect(() => {
-        
+
         if (patternDays) {
 
             for (let i = 0; i < patternDays.length; i++) {
@@ -132,172 +157,187 @@ export default function CalendarGrid(
                 }
             }
         }
-    }, [tempday])
-
-    useEffect(() => {
-        if (startDateOn && endDateOn) {
-            let ignoreon = {
-                startday: +startDateOn.format('DD'),
-                startmonth: +startDateOn.format('MM'),
-                startyear: +startDateOn.format('YYYY'),
-                endday: +endDateOn.format('DD'),
-                endmonth: +endDateOn.format('MM'),
-                endyear: +endDateOn.format('YYYY')
+        if (ignoreOffDays) {
+            for (let i = 0; i < ignoreOffDays.length; i++) {
+                let ioffarr = getIgonoreDays(parseDate(ignoreOffDays[i])[0], parseDate(ignoreOffDays[i])[1])
+                console.log('ioffarr: ', ioffarr)
+                setIsRed(prevIsRed => _.uniq(prevIsRed.concat(ioffarr)));
             }
-
-            console.log(!isObjectInArray(ignoreOnDays, ignoreon))
-            if (!isObjectInArray(ignoreOnDays, ignoreon)) {
-                setIgnoreOnDays([...ignoreOnDays, ignoreon])
-            }
-            let arron = getIgonoreDays(startDateOn, endDateOn)
-            let arr = isRed.filter(item => !arron.includes(item))
-            setIsRed(arr)
-
         }
-    }, [startDateOn, endDateOn])
-
-    useEffect(() => {
-        if (startDateOff && endDateOff) {
-            let ignoreoff = {
-                startday: +startDateOff.format('DD'),
-                startmonth: +startDateOff.format('MM'),
-                startyear: +startDateOff.format('YYYY'),
-                endday: +endDateOff.format('DD'),
-                endmonth: +endDateOff.format('MM'),
-                endyear: +endDateOff.format('YYYY')
+        if (ignoreOnDays) {
+            for (let i = 0; i < ignoreOnDays.length; i++) {
+                let ionarr = getIgonoreDays(parseDate(ignoreOnDays[i])[0], parseDate(ignoreOnDays[i])[1])
+                console.log('ionarr: ', ionarr)
+                setIsRed(prevIsRed => prevIsRed.filter(item => !ionarr.includes(item)))
             }
-            if (!isObjectInArray(ignoreOffDays, ignoreoff)) {
-                setIgnoreOffDays([...ignoreOffDays, ignoreoff])
-            }
-            let arroff = getIgonoreDays(startDateOff, endDateOff)
-            let arr = arroff.filter(item => !isRed.includes(item))
-            setIsRed(isRed.concat(arr))
-
         }
-    }, [startDateOff, endDateOff])
+        
+    }
+    , [tempday])
 
-    useEffect(() => {
-        console.log(`isRed:${isRed}`)
-    }, [isRed])
+useEffect(() => {
+    if (startDateOn && endDateOn) {
+        let ignoreon = {
+            startday: +startDateOn.format('DD'),
+            startmonth: +startDateOn.format('MM'),
+            startyear: +startDateOn.format('YYYY'),
+            endday: +endDateOn.format('DD'),
+            endmonth: +endDateOn.format('MM'),
+            endyear: +endDateOn.format('YYYY')
+        }
 
-    useEffect(() => {
-        console.log('patternDays:')
-        console.log(patternDays)
-    }, [patternDays])
+        console.log(!isObjectInArray(ignoreOnDays, ignoreon))
+        if (!isObjectInArray(ignoreOnDays, ignoreon)) {
+            setIgnoreOnDays([...ignoreOnDays, ignoreon])
+        }
+        let arron = getIgonoreDays(startDateOn, endDateOn)
+        let arr = isRed.filter(item => !arron.includes(item))
+        setIsRed(arr)
+    }
+}, [startDateOn, endDateOn])
 
-    useEffect(() => {
-        console.log('ignoreOnDays:')
-        console.log(ignoreOnDays)
-    }, [ignoreOnDays])
+useEffect(() => {
+    if (startDateOff && endDateOff) {
+        let ignoreoff = {
+            startday: +startDateOff.format('DD'),
+            startmonth: +startDateOff.format('MM'),
+            startyear: +startDateOff.format('YYYY'),
+            endday: +endDateOff.format('DD'),
+            endmonth: +endDateOff.format('MM'),
+            endyear: +endDateOff.format('YYYY')
+        }
+        if (!isObjectInArray(ignoreOffDays, ignoreoff)) {
+            setIgnoreOffDays([...ignoreOffDays, ignoreoff])
+        }
+        let arroff = getIgonoreDays(startDateOff, endDateOff)
+        let arr = arroff.filter(item => !isRed.includes(item))
+        setIsRed(isRed.concat(arr))
+    }
+}, [startDateOff, endDateOff])
 
-    useEffect(() => {
-        console.log('ignoreOffDays:')
-        console.log(ignoreOffDays)
-    }, [ignoreOffDays])
+
+useEffect(() => {
+    console.log(`isRed:${isRed}`)
+}, [isRed])
+
+useEffect(() => {
+    console.log('patternDays:')
+    console.log(patternDays)
+}, [patternDays])
+
+useEffect(() => {
+    console.log('ignoreOnDays:')
+    console.log(ignoreOnDays)
+}, [ignoreOnDays])
+
+useEffect(() => {
+    console.log('ignoreOffDays:')
+    console.log(ignoreOffDays)
+}, [ignoreOffDays])
 
 
-    const OnClickCell2 = (weekday, day) => {
-        let elems = document.querySelector('.selectignore:checked');
+const OnClickCell2 = (weekday, day) => {
+    let elems = document.querySelector('.selectignore:checked');
 
-        if (document.querySelector('.selectignore:checked')) {
-            let selectmode = +elems.value
-            if (selectmode === 1) {
-                console.log("Задаем паттерн")
-                let arrp = getPatternDays(weekday, daysArray)
-                setIsRed(prevIsRed => _.uniq(prevIsRed.concat(arrp)));
+    if (document.querySelector('.selectignore:checked')) {
+        let selectmode = +elems.value
+        if (selectmode === 1) {
+            console.log("Задаем паттерн")
+            let arrp = getPatternDays(weekday, daysArray)
+            setIsRed(prevIsRed => _.uniq(prevIsRed.concat(arrp)));
 
-                let pattern = {
-                    year: +tempday.format('YYYY'),
-                    month: +tempday.format('MM'),
-                    day: +weekday
-                }
-                console.log(pattern)
-                console.log(!isObjectInArray(patternDays, pattern))
-                if (!isObjectInArray(patternDays, pattern)) {
-                    setPatternDays([...patternDays, pattern])
-                }
+            let pattern = {
+                year: +tempday.format('YYYY'),
+                month: +tempday.format('MM'),
+                day: +weekday
+            }
+            console.log(pattern)
+            console.log(!isObjectInArray(patternDays, pattern))
+            if (!isObjectInArray(patternDays, pattern)) {
+                setPatternDays([...patternDays, pattern])
+            }
 
-            } else if (selectmode === 2) {
-                console.log("Задаем паттерн")
-                let arryp = getYearPatternDays(weekday, daysArray)
-                setIsRed(prevIsRed => _.uniq(prevIsRed.concat(arryp)));
+        } else if (selectmode === 2) {
+            console.log("Задаем паттерн")
+            let arryp = getYearPatternDays(weekday, daysArray)
+            setIsRed(prevIsRed => _.uniq(prevIsRed.concat(arryp)));
 
-                let ypattern = {
-                    year: +tempday.format('YYYY'),
-                    month: +13,
-                    day: +weekday
-                }
-                console.log(!isObjectInArray(patternDays, ypattern))
-                console.log(ypattern)
-                if (!isObjectInArray(patternDays, ypattern)) {
-                    setPatternDays([...patternDays, ypattern])
-                }
+            let ypattern = {
+                year: +tempday.format('YYYY'),
+                month: +13,
+                day: +weekday
+            }
+            console.log(!isObjectInArray(patternDays, ypattern))
+            console.log(ypattern)
+            if (!isObjectInArray(patternDays, ypattern)) {
+                setPatternDays([...patternDays, ypattern])
+            }
 
-            } else if (selectmode === 3) {
-                console.log("Задаем рабочие")
-                if (!startDateOn) {
-                    console.log("Первый день1:", day)
-                    setStartDateOn(day);
-                } else if (startDateOn && !endDateOn) {
-                    console.log("Второй день:", day)
-                    setEndDateOn(day);
-                } else {
-                    console.log("Первый день2:", day)
-                    setStartDateOn(day);
-                    setEndDateOn(null);
-                }
+        } else if (selectmode === 3) {
+            console.log("Задаем рабочие")
+            if (!startDateOn) {
+                console.log("Первый день1:", day)
+                setStartDateOn(day);
+            } else if (startDateOn && !endDateOn) {
+                console.log("Второй день:", day)
+                setEndDateOn(day);
+            } else {
+                console.log("Первый день2:", day)
+                setStartDateOn(day);
+                setEndDateOn(null);
+            }
 
-            } else if (selectmode === 4) {
-                console.log("Задаем выходные")
+        } else if (selectmode === 4) {
+            console.log("Задаем выходные")
 
-                if (!startDateOff) {
-                    console.log("Первый день1:", day)
-                    setStartDateOff(day);
-                } else if (startDateOff && !endDateOff) {
-                    console.log("Второй день:", day)
-                    setEndDateOff(day);
-                } else {
-                    console.log("Первый день2:", day)
-                    setStartDateOff(day);
-                    setEndDateOff(null);
-                }
+            if (!startDateOff) {
+                console.log("Первый день1:", day)
+                setStartDateOff(day);
+            } else if (startDateOff && !endDateOff) {
+                console.log("Второй день:", day)
+                setEndDateOff(day);
+            } else {
+                console.log("Первый день2:", day)
+                setStartDateOff(day);
+                setEndDateOff(null);
             }
         }
     }
+}
 
 
 
-    return (
-        <div>
-            <GridWrapper>
-                {
-                    daysArray.map((dayItem) => (
-                        <CellWrapper onClick={() => {
-                            // OnClickCell(dayItem.day(), +dayItem.format('D'));
-                            OnClickCell2(dayItem.day(), dayItem);
-                        }}
+return (
+    <div>
+        <GridWrapper>
+            {
+                daysArray.map((dayItem) => (
+                    <CellWrapper onClick={() => {
+                        // OnClickCell(dayItem.day(), +dayItem.format('D'));
+                        OnClickCell2(dayItem.day(), dayItem);
+                    }}
 
-                            key={dayItem.format('DDMMYYYY')}
-                            isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
-                            isDayRed={isRed.includes(dayItem.format('DDMMYYYY'))}
-                        >
-                            <RowCell id={dayItem.format('DDMMYYYY')} flexend>
-                                <DayWrapper
-                                    isCurentDay={dayItem.format('DDMMYYYY') === moment().format('DDMMYYYY')}
+                        key={dayItem.format('DDMMYYYY')}
+                        isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
+                        isDayRed={isRed.includes(dayItem.format('DDMMYYYY'))}
+                    >
+                        <RowCell id={dayItem.format('DDMMYYYY')} flexend>
+                            <DayWrapper
+                                isCurentDay={dayItem.format('DDMMYYYY') === moment().format('DDMMYYYY')}
+                            >
+                                <CurrentDay
+                                    isThisMonth={dayItem.format('M') === daysArray[20].format('M')}
                                 >
-                                    <CurrentDay
-                                        isThisMonth={dayItem.format('M') === daysArray[20].format('M')}
-                                    >
-                                        {dayItem.format('D')}
-                                    </CurrentDay>
-                                </DayWrapper>
-                            </RowCell>
-                        </CellWrapper>
-                    ))
-                }
-            </GridWrapper>
+                                    {dayItem.format('D')}
+                                </CurrentDay>
+                            </DayWrapper>
+                        </RowCell>
+                    </CellWrapper>
+                ))
+            }
+        </GridWrapper>
 
 
-        </div>
-    )
+    </div>
+)
 }
